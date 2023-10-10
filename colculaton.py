a8f61a072dd335899f1091a763dbdf8aeeab57df
povilas taraikavicius <ptpecka@gmail.com>
2023-10-10 20:01:37 +0300

    create calculator

---
+++ colculaton.py
@@ -0,0 +1,13 @@
+print("calculator: ")
+a = int(input("enter your first number: "))
+c = input("enter simbol: ")
+b = int(input("enter your second number: "))
+
+if c == "+":
+    print(a+b)
+if c == "-":
+    print(a-b)
+if c == "*":
+    print(a*b)
+if c == "/":
+    print(a/b)
